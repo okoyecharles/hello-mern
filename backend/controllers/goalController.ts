896482b2b27d166a6d6925b37d8ff97f9a5c64fb
Charles <okoyecharles509@gmail.com>
2022-12-24 21:59:00 +0100

    Setup controller for backend

--- backend/controllers/goalController.ts
+++ backend/controllers/goalController.ts
@@ -1,27 +1,33 @@
+import expressAsyncHandler from "express-async-handler"
 // @desc    Get goals
 // @route   GET /api/goals
 // @access  Private
-export const getGoals = (req: any, res: any) => {
-  res.status(200).json({message: 'Get goals'})
-}
+export const getGoals = expressAsyncHandler(async (req: any, res: any) => {
+  res.status(200).json({ message: 'Get goals' })
+})
 
 // @desc    Set goals
 // @route   POST /api/goals
 // @access  Private
-export const createGoal = (req: any, res: any) => {
-  res.status(201).json({message: 'Set goal'})
-}
+export const createGoal = expressAsyncHandler(async (req: any, res: any) => {
+  if (!req.body.text) {
+    res.status(400)
+    throw new Error('Please add a text field ')
+  }
+
+  res.status(201).json({ message: 'Set goal' })
+})
 
 // @desc    Update goals
 // @route   PUT /api/goals/:id
 // @access  Private
-export const updateGoal = (req: any, res: any) => {
-  res.status(200).json({message: `Update goal ${req.params.id}`})
-}
+export const updateGoal = expressAsyncHandler(async (req: any, res: any) => {
+  res.status(200).json({ message: `Update goal ${req.params.id}` })
+})
 
 // @desc    Delete goals
 // @route   DELETE /api/goals/:id
 // @access  Private
-export const deleteGoal = (req: any, res: any) => {
-  res.status(200).json({message: `Delete goal ${req.params.id}`})
-}
+export const deleteGoal = expressAsyncHandler(async (req: any, res: any) => {
+  res.status(200).json({ message: `Delete goal ${req.params.id}` })
+})
--- backend/server.ts
+++ backend/server.ts
@@ -1,10 +1,17 @@
 import dotenv from 'dotenv';
 dotenv.config();
+import { errorHandler } from './middleware/errorMiddleware';
 import express from 'express';
 import router from './routes/goalRoutes';
 
 const port = process.env.PORT;
 const app = express()
 
-app.use('/api/goals', router)
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.use('/api/goals', router);
+
+app.use(errorHandler);
+
 app.listen(port, () => console.log(`Server started on ${port}`))
